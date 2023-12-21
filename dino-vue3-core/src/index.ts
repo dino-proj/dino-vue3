add73ce935908493d59de429ddaad896337e526b
Cody Lu <tuuboo@gmail.com>
2023-12-22 00:39:55 +0800

    Add setupDinoCore function to configure DinoCore with message and API settings

--- dino-vue3-core/src/index.ts
+++ dino-vue3-core/src/index.ts
@@ -1,12 +1,35 @@
 // Copyright 2023 dinosdev.cn.
 // SPDX-License-Identifier: MIT
 
+import { ApiConfig, ApiReqeust, setupApi } from './api'
+import { MessageFun, MessageHandler, setupMessage } from './message'
+
 export * from './common'
 export * from './auth'
 export * from './utils'
 export * from './message'
 export * from './api'
 
-export const setupDinoCore = (): void => {
-  // TODO
+export interface DinoCoreConfig {
+  message?: MessageHandler
+  api?: ApiConfig
+  apiRequest: (api: ApiConfig) => ApiReqeust
+}
+
+const nopMessageFun: MessageFun = () => {
+  console.warn('Please use `setupDinoCore({msgHandlers:H})` or `provide(MessageSymbol)` first before call `useMessage()`')
+}
+const nopMessageHandler: MessageHandler = {
+  success: nopMessageFun,
+  info: nopMessageFun,
+  warning: nopMessageFun,
+  error: nopMessageFun
+}
+
+export const setupDinoCore = (config: DinoCoreConfig): void => {
+  // setup the message handler
+  setupMessage(config.message ?? nopMessageHandler)
+
+  // setup the api request
+  setupApi(config.apiRequest, config.api)
 }
