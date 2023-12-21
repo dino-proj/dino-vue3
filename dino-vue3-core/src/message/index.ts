b3b9fc5f9a1f6bb1aacd0142454787b056ea7ca6
Cody Lu <tuuboo@gmail.com>
2023-12-22 00:15:32 +0800

    Refactor message handling and add injection key

--- dino-vue3-core/src/message/index.ts
+++ dino-vue3-core/src/message/index.ts
@@ -3,12 +3,14 @@
 
 import { isNil } from 'lodash-es'
 
+import { InjectionKey, inject, provide } from '@vue/runtime-core'
+
 export declare type MessageFun = (msg: string, options?: any) => void
 
 /**
  * 消息提示接口
  */
-export interface Message {
+export interface MessageHandler {
   success: MessageFun
   info: MessageFun
   warning: MessageFun
@@ -16,33 +18,39 @@ export interface Message {
 }
 
 /**
+ * 消息提示接口Symbol, 用于InjectionKey
+ */
+export const MessageSymbol: InjectionKey<MessageHandler> = Symbol()
+
+/**
  * 消息类型
  */
-export type MessageType = `${string & keyof Message}`
+export type MessageType = `${string & keyof MessageHandler}`
 
-let msg: Message
+let msg: MessageHandler
 
 /**
  * 使用消息函数，用来显示提示消息
  * @param msgType 消息类型
  * @returns
  */
-export function useMessage(): Message
+export function useMessage(): MessageHandler
 export function useMessage(msgType: MessageType): MessageFun
-export function useMessage(msgType?: MessageType): MessageFun | Message {
-  if (isNil(msg)) {
+export function useMessage(msgType?: MessageType): MessageFun | MessageHandler {
+  const _msg = inject(MessageSymbol, msg)
+  if (isNil(_msg)) {
     throw new Error('Please use `setupMessage()` or `provide("")` first before call `useMessage()`')
   }
   if (!isNil(msgType)) {
-    return msg[msgType]
+    return _msg[msgType]
   }
-  return msg
+  return _msg
 }
 
 /**
  * 设置消息处理对象
  * @param _msg
  */
-export const setupMessage = (_msg: Message): void => {
+export const setupMessage = (_msg: MessageHandler): void => {
   msg = _msg
 }
