9197f26ca136377459a88f13eb08b19956b90c5c
Cody Lu <tuuboo@gmail.com>
2024-02-01 08:05:40 +0800

    移除拦截器

--- dino-vue3-core/src/request/axios.ts
+++ dino-vue3-core/src/request/axios.ts
@@ -1,13 +1,8 @@
 // Copyright 2023 dinosdev.cn.
 // SPDX-License-Identifier: MIT
 
-import axios, { AxiosError, AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios'
-import { isEmpty } from 'lodash-es'
-import { stringify } from 'qs'
-import { ApiConfig, ApiResponse, HttpRequest, RequestProvider } from '../api'
-import { Tenant } from '../common'
-import { useMessage } from '../message'
-import { asArray } from '../utils'
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+import { ApiConfig, HttpRequest, RequestProvider } from '../api'
 
 export const axiosWithAdapter = (adapter?: AxiosRequestConfig['adapter']): RequestProvider => {
   return (config: ApiConfig): HttpRequest => {
@@ -20,73 +15,6 @@ export const axiosWithAdapter = (adapter?: AxiosRequestConfig['adapter']): Reque
       proxy: config.proxy,
       adapter
     })
-
-    const successCode: number[] = asArray(config.successCode)
-    const needLoginCode: number[] = asArray(config.needLoginCode)
-
-    // request拦截器
-    service.interceptors.request.use(
-      (conf) => {
-        if (conf.method === 'post' && conf.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
-          conf.data = stringify(conf.data)
-        }
-
-        const tenant: Tenant = config.tenant()
-
-        if (!isEmpty(tenant)) {
-          conf.url = conf.url.replace('{tenant}', tenant.id)
-        }
-
-        const authToken = config.authToken()
-        if (authToken) {
-          conf.headers[authToken.authHeaderName] = authToken.authPayload
-        }
-        return conf
-      },
-      (error: AxiosError) => {
-        // Do something with request error
-        console.log(error) // for debug
-        Promise.reject(error)
-      }
-    )
-
-    // response 拦截器
-    service.interceptors.response.use(
-      (response: AxiosResponse): Promise<AxiosResponse> => {
-        return new Promise<AxiosResponse>((resolve, reject) => {
-          const data = response.data as ApiResponse<any>
-
-          if (successCode.indexOf(data.code) >= 0) {
-            resolve(response)
-          } else if (needLoginCode.indexOf(data.code) >= 0) {
-            return config
-              .autoLogin()
-              .then(() => {
-                service(response.config)
-                  .then((resp) => {
-                    resolve(resp)
-                  })
-                  .catch((reason) => {
-                    reject(reason)
-                  })
-              })
-              .catch((reason) => {
-                useMessage().error(reason.message)
-                reject(reason)
-              })
-          } else {
-            useMessage().error(data.msg)
-            return reject(response)
-          }
-        })
-      },
-      (error: AxiosError) => {
-        console.log(error.code, 'err:' + error) // for debug
-
-        useMessage().error('网络错误，请检查网络连接是否正常！')
-        return Promise.reject(error)
-      }
-    )
 
     return service
   }
