5941ea39569dcdae1297c0297b480807f2252af9
Cody Lu <tuuboo@gmail.com>
2024-02-20 20:38:50 +0800

    更新刷新令牌的API定义

--- dino-vue3-core/src/auth/api.ts
+++ dino-vue3-core/src/auth/api.ts
@@ -1,12 +1,21 @@
 // Copyright 2024 dinodev.cn.
 // SPDX-License-Identifier: MIT
 
-import { definePostApi } from 'src/api'
+import { IdKeyType, User } from '../common'
+import { definePostApi } from '../api'
 import { DinoLoginAuthInfo, LoginByRefreshTokenApi } from './types'
 
-export const defineRefreshTokenApi = (path?: string, service?: string): LoginByRefreshTokenApi => {
-  const post = definePostApi<DinoLoginAuthInfo, { refreshToken: string }>({
-    url: path || '/login/refresh-token',
+/**
+ * 定义刷新令牌的 API
+ * @template K - ID 类型
+ * @template U - 用户类型
+ * @param {string} path - API 路径
+ * @param {string} service - 服务名称
+ * @returns {LoginByRefreshTokenApi<K, U>} - 刷新令牌的登录 API
+ */
+export const defineRefreshTokenApi = <K extends IdKeyType = string, U extends User<K> = User<K>>(path: string, service?: string): LoginByRefreshTokenApi<K, U> => {
+  const post = definePostApi<DinoLoginAuthInfo<K, U>, { refreshToken: string }>({
+    url: path,
     service,
     withToken: false
   })
